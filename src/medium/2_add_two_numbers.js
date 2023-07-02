6a85d79ca69cb628e10d1fb14b52de90fb760942
Ilia Poliakov <wwwtrayfrost@gmail.com>
2023-07-02 20:01:08 +0400

    Medium 2: Add Two Numbers; solution

--- src/medium/2_add_two_numbers.js
+++ src/medium/2_add_two_numbers.js
@@ -1,4 +1,5 @@
 /*
+	https://leetcode.com/problems/add-two-numbers/
 	You are given two non-empty linked lists representing two non-negative integers.
 	The digits are stored in reverse order, and each of their nodes contains a single digit.
 	Add the two numbers and return the sum as a linked list.
@@ -24,8 +25,25 @@ class ListNode {
  * @return {ListNode}
  */
 const addTwoNumbers = function(l1, l2) {
-
+	let carry = 0;
+	let currListNode1 = l1;
+	let currListNode2 = l2;
+	let result = new ListNode()
+	let resultCurrent = result;
+	while(true) {
+		let sum = (currListNode1?.val || 0) + (currListNode2?.val || 0) + carry;
+		resultCurrent.val = sum % 10;
+		carry = sum > 9 ? 1 : 0;
+		if (currListNode1?.next || currListNode2?.next || carry) {
+			resultCurrent.next = new ListNode();
+			resultCurrent = resultCurrent.next;
+			currListNode1 = currListNode1?.next;
+			currListNode2 = currListNode2?.next;
+		} else break;
+	}
+	return result;
 };
+
 module.exports = {
 	ListNode,
 	addTwoNumbers
--- src/medium/2_add_two_numbers.spec.js
+++ src/medium/2_add_two_numbers.spec.js
@@ -1,21 +1,6 @@
 const { addTwoNumbers, ListNode } = require('./2_add_two_numbers');
 
-test('Checks 2 Add Two numbers task', () => {
-  const l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-  const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-  expect(addTwoNumbers(l1, l2)).toStrictEqual(new ListNode(7, new ListNode(0, new ListNode(8))));
-
-  const l3 = new ListNode()
-  const l4 = new ListNode()
-  expect(addTwoNumbers(l3, l4)).toStrictEqual(new ListNode());
-
-  const l5 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))))
-  const l6 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
-  const res = new ListNode(8, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1))))))))
-  expect(addTwoNumbers(l5, l6)).toStrictEqual(res);
-});
-
-test('Checks 2 Add Two numbers task (fromArray method)', () => {
+test('2 | Add Two Numbers', () => {
   const l1 = ListNode.fromArray([2,4,3]);
   const l2 = ListNode.fromArray([5,6,4]);
   const res1 = ListNode.fromArray([7,0,8]);
@@ -26,21 +11,7 @@ test('Checks 2 Add Two numbers task (fromArray method)', () => {
   expect(addTwoNumbers(l3, l4)).toStrictEqual(new ListNode());
 
   const l5 = ListNode.fromArray([9,9,9,9,9,9,9]);
-  const l6 = ListNode.fromArray([9,9,9,9,9]);
+  const l6 = ListNode.fromArray([9,9,9,9]);
   const res2 = ListNode.fromArray([8,9,9,9,0,0,0,1]);
   expect(addTwoNumbers(l5, l6)).toStrictEqual(res2);
 });
-
-/*
-Input: l1 = [2,4,3], l2 = [5,6,4]
-Output: [7,0,8]
-Explanation: 342 + 465 = 807.
-Example 2:
-
-Input: l1 = [0], l2 = [0]
-Output: [0]
-Example 3:
-
-Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-Output: [8,9,9,9,0,0,0,1]
- */
